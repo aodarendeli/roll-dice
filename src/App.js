018715cbb482d800a4381fa5004bdcb3f6cc08ef
mmezgin <mm.ezgn@gmail.com>
2022-09-15 18:48:42 +0300

    fd

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import './App.css';
 import Sidebar from './components/Sidebar/Sidebar';
+import {createContext,useState,useEffect} from 'react'
+
+export const ThemeContext = createContext(null);
 
 function App() {
+  const [theme, setTheme] = useState("dark");
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+  };
   return (
-      <>
-      <Sidebar />
-      <div className='text-center'>
-        merhaba yarr
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div className="App" id={theme}>
+        <div 
+        onClick={toggleTheme}
+        >anaa</div>
+        <div className="switch">
+          <Sidebar />
+        </div>
       </div>
-      </>
+    </ThemeContext.Provider>
+
   );
 }
 
